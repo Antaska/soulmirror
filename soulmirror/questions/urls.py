c908093f858063ea5b6b9f61aeb908e66ed9d635
Iván Anta Porto <ianta@nevian.es>
2025-01-02 00:29:03 +0100

    Added functionality for getting new unique questions

--- soulmirror/questions/urls.py
+++ soulmirror/questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import cargar_preguntas
+from .views import cargar_preguntas, pregunta_diaria
 
 urlpatterns = [
     path("cargar-preguntas/", cargar_preguntas, name="cargar_preguntas"),
+    path("pregunta-diaria/", pregunta_diaria, name="pregunta_diaria"),
 ]
--- soulmirror/questions/views.py
+++ soulmirror/questions/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.utils.timezone import now
 from .models import Question
-
+import random
 # Create your views here.
 
 def cargar_preguntas(request):
@@ -45,3 +46,19 @@ def cargar_preguntas(request):
         messages.warning(request, f"{ignoradas} líneas ignoradas (duplicadas o mal formateadas).")
     
     return render(request, "cargar_preguntas.html")
+
+def pregunta_diaria(request):
+    ultima_pregunta = Question.objects.filter(respondida__isnull=False).order_by('-respondida').first()
+    ultima_tematica = ultima_pregunta.tematica if ultima_pregunta else None
+
+    preguntas_disponibles = Question.objects.filter(
+        respondida__isnull=True
+    ).exclude(tematica=ultima_tematica)
+
+    if preguntas_disponibles.exists():
+        pregunta = random.choice(preguntas_disponibles)
+        pregunta.respondida_date = now()
+        pregunta.save()
+        return render(request, "pregunta_diaria.html",{"pregunta":pregunta})
+    else:
+        return render(request, "no_preguntas.html")
